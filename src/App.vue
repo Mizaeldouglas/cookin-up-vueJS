6eedf29dc911dc6a73411493bb99ef788c0677c0
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-11-13 20:17:13 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+import ConteudoPrincial from "./components/ConteudoPrincial.vue";
+
+export default {
+  components: { Banner, ConteudoPrincial },
+};
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincial/>
+</template>
